fef2ab47073fd58c10b1921482034a2dd6f7cfc7
jagadishdatta <jagadishdatta5@gmail.com>
2021-05-10 14:18:38 +0530

    update app.js
    app js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Jumbotron } from 'reactstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Content from './components/Content ';
+import Content from './components/Content';
 function App() {
   return (
     <div>
